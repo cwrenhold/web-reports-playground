e2c19c153092d821813dc48c0ba28755c6ff795a
Catherine Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2023-09-12 14:25:36 +0100

    Added options handling for subjects and filter breakdown.

--- BackendCSharp/Pages/GradeBreakdown.cshtml.cs
+++ BackendCSharp/Pages/GradeBreakdown.cshtml.cs
@@ -15,40 +15,93 @@ public class GradeBreakdownModel : PageModel
         _logger = logger;
     }
 
+    [FromQuery]
     public int? SelectedSubjectId { get; set; }
 
+    [FromQuery]
     public int? SelectedFilterId { get; set; }
 
     public List<Subject> Subjects { get; private set; } = new List<Subject>();
 
     public List<Filter> Filters { get; private set; } = new List<Filter>();
 
-    public List<ReportDataItem> ReportData { get; private set; }
+    public List<ReportDataItem> ReportData { get; private set; } = new();
+
+    public List<IGrouping<string?, ReportDataItem>> GroupReportData { get; private set; } = new();
 
     public void OnGet()
     {
         this.Subjects = this._playgroundContext.Subjects.OrderBy(x => x.Name).ToList();
         this.Filters = this._playgroundContext.Filters.OrderBy(x => x.Name).ToList();
 
-        var results =
-            from sg in this._playgroundContext.StudentGrades
-            join grd in this._playgroundContext.Grades on sg.GradeId equals grd.Id
-            group sg by sg.GradeId into g
-            select new ReportDataItem
-            {
-                GradeId = g.Key,
-                Label = g.First().Grade.Text,
-                Count = g.Count()
-            };
+        var students = this._playgroundContext.Students.AsQueryable();
+        var studentGrades = this._playgroundContext.StudentGrades.AsQueryable();
 
-        this.ReportData = results.ToList();
+        if (this.SelectedSubjectId is not null)
+        {
+            studentGrades = studentGrades
+                .Where(sg => sg.SubjectId == this.SelectedSubjectId);
+        }
+
+        IQueryable<ReportDataItem> results;
+        
+        if (this.SelectedFilterId is null)
+        {
+            results = 
+                from sg in studentGrades
+                join std in students on sg.StudentId equals std.Id
+                join grd in this._playgroundContext.Grades on sg.GradeId equals grd.Id
+                group sg by sg.GradeId into g
+                select new ReportDataItem
+                {
+                    FilterValue = null,
+                    GradeId = g.Key,
+                    Label = g.First().Grade.Text,
+                    Points = g.First().Grade.Points,
+                    Count = g.Count()
+                };
+        }
+        else
+        {
+            results = 
+                from sg in studentGrades
+                join std in students on sg.StudentId equals std.Id
+                join grd in this._playgroundContext.Grades on sg.GradeId equals grd.Id
+                join sf in this._playgroundContext.StudentFilters.Where(sf => sf.FilterId == this.SelectedFilterId)
+                    on sg.StudentId equals sf.StudentId
+                join fv in this._playgroundContext.FilterValues on sf.FilterValueId equals fv.Id
+                group sg by new { sf.FilterValueId, fv.Text, sg.GradeId } into g
+                select new ReportDataItem
+                {
+                    FilterValue = g.Key.Text,
+                    GradeId = g.Key.GradeId,
+                    Label = g.First().Grade.Text,
+                    Points = g.First().Grade.Points,
+                    Count = g.Count()
+                };
+        }
+
+        this.ReportData = results
+            .ToList()
+            .OrderBy(rd => rd.FilterValue)
+            .ThenByDescending(rd => rd.Points != null)
+            .ThenByDescending(rd => rd.Points)
+            .ToList();
+
+        this.GroupReportData = this.ReportData
+            .GroupBy(rd => rd.FilterValue)
+            .ToList();
     }
 
     public class ReportDataItem
     {
+        public string? FilterValue { get; set; }
+
         public int GradeId { get; set; }
 
         public string Label { get; set; } = string.Empty;
+
+        public decimal? Points { get; set; }
 
         public int Count { get; set; }
     }
