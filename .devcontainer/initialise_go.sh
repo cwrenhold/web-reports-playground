bdf43ca8822c6b7b140fe13759a8affc0de572dc
Catherine Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2023-09-30 22:08:53 +0100

    Added support for tinygo, but this is not yet implemented in the JavaScript side of the Go version.

--- .devcontainer/initialise_go.sh
+++ .devcontainer/initialise_go.sh
@@ -2,3 +2,6 @@
 
 # Copy wasm_exec.js to the public/dist folder
 cp $(go env GOROOT)/misc/wasm/wasm_exec.js ./FrontendGo/public/dist
+
+# Copy Tiny Go tiny_wasm_exec.js to the public/dist folder
+cp $(tinygo env TINYGOROOT)/targets/wasm_exec.js ./FrontendGo/public/dist/tiny_wasm_exec.js
